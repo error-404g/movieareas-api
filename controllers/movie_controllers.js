6bf7ff9dc41b22060140ca4904ff18813db9ffe2
Giri <gd02675@gmail.com>
2025-08-27 12:04:59 +0530

    model

--- controllers/movie_controllers.js
+++ controllers/movie_controllers.js
@@ -1,14 +1,26 @@
+import Movie from '../models/movie_model.js';
+
 export const movieIndex = (req, res) => {
   res.send('Get all movie list');
 };
 
-export const movieCreate = (req, res) => {
+export const movieCreate = async (req, res) => {
   // id, title, description
-
-  console.log(req.body);
   // create the movie info.
 
-  return res.json(req.body);
+  // Validate your data.  
+  const newMovie = new Movie({
+    title: req.body.title,
+    desc: req.body.desc
+  });
+  
+
+  try {
+    const movie = await newMovie.save();
+    return res.status(201).json(movie);
+  } catch (error){
+    return res.status(400).json({message: error.message});
+  }
 };
 
 export const movieUpdate = (req, res) => {
---
+++ models/movie_model.js
@@ -0,0 +1,13 @@
+import { Schema, model } from "mongoose";
+
+// Write the schema
+const schema = new Schema({
+  title: String,
+  desc: String
+})
+
+// Create your model
+
+const Movie = model("Movie", schema);
+
+export default Movie;
